2d77d1bd7c17c6b31aee26c8fda8ff7865b5a112
sam <letunhatkong@gmail.com>
2026-01-30 16:23:57 +0700

    Slope: base woods

--- js/main.js
+++ js/main.js
@@ -1,35 +1,67 @@
-const canvas = document.getElementById("renderCanvas"); // Get the canvas element
-const engine = new BABYLON.Engine(canvas, true); // Generate the BABYLON 3D engine
+const canvas = document.getElementById("renderCanvas");
+const engine = new BABYLON.Engine(canvas, true); // BABYLON 3D engine
 
-const createScene = function () {
-    // Creates a basic Babylon Scene object
-    const scene = new BABYLON.Scene(engine);
+const BBL = {
+    scene: null,
+    initsScene() {
+        this.scene = new BABYLON.Scene(engine);
+        this.cameraSetup();
+        this.lightSetup();
+        this.groundSetup();
+        this.wallSetup();
+        return this.scene;
+    },
 
-    // Creates and positions a free camera
-    const camera = new BABYLON.FreeCamera("camera1", new BABYLON.Vector3(0, 5, -10), scene);
-    camera.setTarget(BABYLON.Vector3.Zero());  // Targets the camera to scene origin
-    camera.attachControl(canvas, true);   // This attaches the camera to the canvas
+    cameraSetup() {
+        // Scroll support
+        const camera = new BABYLON.ArcRotateCamera(
+            "camera",
+            Math.PI / 2,   // alpha
+            Math.PI / 4,   // beta
+            30,            // radius (target distance)
+            BABYLON.Vector3.Zero(), // target
+            this.scene
+        );
+        camera.wheelPrecision = 20; // Zoom -speed
+        camera.attachControl(canvas, true);   // This attaches the camera to the canvas
+    },
+    lightSetup() {
+        // Creates a light, aiming 0,1,0 - to the sky
+        const light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(0, 1, 0), this.scene);
+        light.intensity = 0.7;   // Dim the light a small amount - 0 to 1
+    },
+    groundSetup() {
+        // Built-in 'ground' shape.
+        const ground = BABYLON.MeshBuilder.CreateGround("ground", {width: 12, height: 6}, this.scene);
+    },
+    buildShape() {
+        // Built-in 'sphere' shape.
+        const sphere = BABYLON.MeshBuilder.CreateSphere("sphere", {diameter: 4, segments: 4}, this.scene);
+        sphere.position.y = 1;   // Move the sphere upward 1/2 its height
+    },
+    wallSetup() {
+        const wall1 = BABYLON.MeshBuilder.CreateBox("wall1", {width:12, height:6, depth:0.1}, this.scene);
+        wall1.position.z = -2;
+        const wall2 = wall1.clone("wall2");
+        wall2.position.z = 2;
+        const wall3 = BABYLON.MeshBuilder.CreateBox("wall3", {width:0.1, height:2, depth:4}, this.scene);
+        wall3.position.x = -2;
+        const wall4 = wall3.clone("wall4");
+        wall4.position.x = 2;
+    }
 
-    // Creates a light, aiming 0,1,0 - to the sky
-    const light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(0, 1, 0), scene);
-
-    light.intensity = 0.7;   // Dim the light a small amount - 0 to 1
-    const sphere = BABYLON.MeshBuilder.CreateSphere("sphere", {diameter: 2, segments: 32}, scene); // Built-in 'sphere' shape.
-    sphere.position.y = 1;   // Move the sphere upward 1/2 its height
-
-    // Built-in 'ground' shape.
-    const ground = BABYLON.MeshBuilder.CreateGround("ground", {width: 12, height: 6}, scene);
-    return scene;
-};
+}
 
 
-const scene = createScene(); //Call the createScene function
+const scene = BBL.initsScene();
 
 // Register a render loop to repeatedly render the scene
 engine.runRenderLoop(function () {
     scene.render();
 });
+
 // Watch for browser/canvas resize events
 window.addEventListener("resize", function () {
     engine.resize();
 });
+
---
+++ js/slope.js
@@ -0,0 +1,107 @@
+const canvas = document.getElementById("renderCanvas");
+const engine = new BABYLON.Engine(canvas, true); // BABYLON 3D engine
+
+const BBL = {
+    cameraSetup() {
+        // Scroll support
+        const camera = new BABYLON.ArcRotateCamera(
+            "camera",
+            Math.PI / 2,   // alpha
+            Math.PI / 4,   // beta
+            30,            // radius (target distance)
+            BABYLON.Vector3.Zero(), // target
+            this.scene
+        );
+        camera.wheelPrecision = 50; // Zoom -speed
+        camera.attachControl(canvas, true);
+    },
+    createWoodBar(w = 8, h = 0.2, d = 0.3, x = 0, y = 0, z = 0) {
+        const woodMaterial = new BABYLON.StandardMaterial("woodMat", this.scene);
+        woodMaterial.diffuseColor = new BABYLON.Color3(1.22, 0.98, 0.61);
+        const plank = BABYLON.MeshBuilder.CreateBox(
+            "plank",
+            {width: w, height: h, depth: d},
+            this.scene
+        );
+        plank.position.x = x;
+        plank.position.y = y;
+        plank.position.z = z;
+        plank.material = woodMaterial;
+    },
+
+    groundSetup() {
+        const ground = BABYLON.MeshBuilder.CreateGround("ground", {width: 10, height: 8, depth: 0.1}, this.scene);
+        const groundMaterial = new BABYLON.StandardMaterial("ground", this.scene);
+        groundMaterial.diffuseColor = new BABYLON.Color3(0.84, 0.84, 0.84); // gray
+        ground.material = groundMaterial;
+    },
+    initsScene() {
+        this.scene = new BABYLON.Scene(engine);
+        this.cameraSetup();
+        this.lightSetup();
+        this.groundSetup();
+        this.woodBarInit();
+        return this.scene;
+    },
+    lightSetup() {
+        // Creates a light, aiming 0,1,0 - to the sky
+        const light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(0, 1, 0), this.scene);
+        light.intensity = 0.7;   // Dim the light a small amount - 0 to 1
+    },
+
+    scene: null,
+
+    wallSetup() {
+        const wall = BABYLON.MeshBuilder.CreateBox("shed", {
+            width: 10,
+            height: 8,
+            depth: 6
+        }, this.scene);
+    },
+
+    woodBarInit() {
+        this.createWoodBar(10, 0.2, 0.3, 0, 0, -3.9); // ground w
+        this.createWoodBar(10, 0.2, 0.3, 0, 0, 3.9);
+        this.createWoodBar(0.3, 0.2, 8, 4.85, 0, 0); // ground h
+        this.createWoodBar(0.3, 0.2, 8, -4.85, 0, 0);
+
+        this.createWoodBar(0.3, 8, 0.3, 4.85, 4, -3.9); // back-left
+        this.createWoodBar(0.3, 8, 0.3, -4.85, 4, -3.9); // back-right
+        this.createWoodBar(0.2, 8, 0.3, -2.9, 4, -3.9);
+        this.createWoodBar(0.2, 8, 0.3, -0.9, 4, -3.9);
+        this.createWoodBar(0.2, 8, 0.3, 2.9, 4, -3.9);
+        this.createWoodBar(0.2, 8, 0.3, 1, 4, -3.9);
+
+        this.createWoodBar(0.3, 6, 0.3, 4.85, 3, 3.9); // front - left
+        this.createWoodBar(0.3, 6, 0.3, -4.85, 3, 3.9); // front - right
+        this.createWoodBar(0.2, 6, 0.3, -2.9, 3, 3.9);
+        this.createWoodBar(0.2, 6, 0.3, -0.9, 3, 3.9);
+
+        this.createWoodBar(0.3, 6, 0.2, 4.85, 3, 2); // side: left
+        this.createWoodBar(0.3, 6, 0.2, 4.85, 3, 0);
+        this.createWoodBar(0.3, 6, 0.2, 4.85, 3, -2);
+        this.createWoodBar(0.3, 6, 0.2, -4.85, 3, 2); // side: right
+        this.createWoodBar(0.3, 6, 0.2, -4.85, 3, 0);
+        this.createWoodBar(0.3, 6, 0.2, -4.85, 3, -2);
+
+        this.createWoodBar(10, 0.2, 0.3, 0, 6, -3.9); // floor: ground w
+        this.createWoodBar(10, 0.2, 0.3, 0, 6, 3.9);
+        this.createWoodBar(0.3, 0.2, 8, 4.85, 6, 0); // floor: ground h
+        this.createWoodBar(0.3, 0.2, 8, -4.85, 6, 0);
+    }
+
+}
+
+
+const scene = BBL.initsScene();
+
+// Register a render loop to repeatedly render the scene
+engine.runRenderLoop(function () {
+    scene.render();
+});
+
+// Watch for browser/canvas resize events
+window.addEventListener("resize", function () {
+    engine.resize();
+});
+
